7f98c0df4554fefdddf934d333981a70cacc1ee4
Кирилл Лебедев <you@example.com>
2024-09-10 03:30:49 +0300

    shit, this all needs a rework (aint doing it though

--- TerminalVersion.py
+++ TerminalVersion.py
@@ -11,49 +11,30 @@ class BJ:
     def __init__(self):
         self.intro = "Welcome to 'Gacha Casino'! (not to be confused with 'Gachi casino')\nIf you want to hear the rules type R\nIf you want to start profiting rapidly type $"
         self.rules = "To win a game of blackjack you have to get more scores than dealer, however if you get higher than 21 that's a loss.\n\
-        Number cards cost exactly their number, face cards all cost 10 and ace can be 1 or 11.\n\
-        The round starts with dealer and the Player both drawing 2 cards. Player's cards are opened immideatly, but dealer opens only one of their cards.\
-        The Player then takes action. They can do the following:\n\
-        Hit (type 'H'), which means drawing 1 more card.\n\
-        Pat (type 'P'), which means not drawing.\n\
-        Split (type 'S'), which means splitting your cards into 2 different games of blackjack (can be done only if Player has 2 cards of the same value).\n\
-        After the Player pats its dealer's turn. Dealer follows specific instructions:\
-        Hit if score dum is less than 16\n\
-        Pat at 17 or higher\n\
-        The deck is standart (for now), 4 suits: hearts, clubs, spades, diamonds, or just H, C, D, S, but they don't matter (for now), no jokers (for now)\n\
-        If you were playing, but forgot something, you can always end the round by inputing 'Stop'\
-        That should be it, have fun, fellow Gambler!"
+Number cards cost exactly their number, face cards all cost 10 and ace can be 1 or 11.\n\
+The round starts with dealer and the Player both drawing 2 cards. Player's cards are opened immideatly, but dealer opens only one of their cards.\
+The Player then takes action. They can do the following:\n\
+Hit (type 'H'), which means drawing 1 more card.\n\
+Pat (type 'P'), which means not drawing.\n\
+Split (type 'S'), which means splitting your cards into 2 different games of blackjack (can be done only if Player has 2 cards of the same value).\n\
+After the Player pats its dealer's turn. Dealer follows specific instructions:\
+Hit if score dum is less than 16\n\
+Pat at 17 or higher\n\
+The deck is standart (for now), 4 suits: hearts, clubs, spades, diamonds, or just H, C, D, S, but they don't matter (for now), no jokers (for now)\n\
+If you were playing, but forgot something, you can always end the round by inputing 'Stop'\
+That should be it, have fun, fellow Gambler!"
         self.player = []
+        self.splitDeck = []
         self.dealer = []
         self.discard = []
         self.deck = ['H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK', 'HA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK', 'CA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK', 'DA']
         # карты игрока | карты диллера | колода | сброс
 
-    def LetsGoGambling(self):
-        yield self.intro
-        while True:
-            action = input()
-            if action not in "Rr$":
-                yield "Excuse me, could you repeat that?"
-            elif action in "Rr":
-                yield self.rules
-            else:
-                while action != "Stop" or action != "sTOP" or action != "stop":
-                    StartRound()
-                    if not self.dealer:
-                        Blackjack()
-                    else:
-                        while action != "Stop" or action != "sTOP" or action != "stop":
-                            action = input()
-                            if action not in 'HPShps' or action not in ['hit', 'Hit', 'hIT', 'Pat', 'pat', 'pAT', 'split', 'Split', 'sPLIT']:
-                                yield "Excuse me, could you repeat that?"
-                            else:
-                                ApplyAction(action)
     
     def StartRound(self):
         l = sorted(sample(range(len(self.deck)), 2))
         PlayerCards = [self.deck[l[0]], self.deck[l[1]]]
-        if PlayerCards[0][1:] in '10JKQ' and PlayerCards[1][1:] == 'A' or PlayerCards[1][1:] in '10JKQ' and PlayerCards[0][1:] == 'A':
+        if PlayerCards[0][1:] in "1JKQ" and PlayerCards[1][1:] == "A" or PlayerCards[1][1:] in "1JKQ" and PlayerCards[0][1:] == "A":
             return
         self.player.append(PlayerCards[0])
         self.player.append(PlayerCards[1])
@@ -62,17 +43,45 @@ class BJ:
         l = l = sorted(sample(range(len(self.deck)), 2))
         self.dealer.append(self.deck[l[0]])
         self.dealer.append(self.deck[l[1]])
-        del self.dealer[l[0]]
-        del self.dealer[l[1] - 1]
-        yield f"Dealer has {self.dealer[0]} and a closed card\n\
-        The Player has {self.player[0]} and {self.player[1]}"
+        del self.deck[l[0]]
+        del self.deck[l[1] - 1]
+        print(f"Dealer has {self.dealer[0]} and a closed card\n\
+The Player has {self.player[0]} and {self.player[1]}\n\
+What's the next move, Captain?")
 
     def ApplyAction(self, action):
-        pass
+        if action in "Hh" or action == "Hit" or action == "hit":
+
+        elif action in "Pp" or action == "Pat" or action == "pat":
+            
+        else:
+
 
     def Blackjack(self):
-        yield "Congrats! That's a blackjack! Pure skill, honestly"
+        print("Congrats! That's a blackjack! Pure skill, honestly")
         # мб нет смысла под крайний случай целую функцию выделять, но кому не пофигу
+
+    def LetsGoGambling(self):
+        print(self.intro)
+        while True:
+            action = input()
+            if action not in "Rr$":
+                print("Excuse me, could you repeat that?")
+            elif action in "Rr":
+                print(self.rules)
+            else:
+                while action != "Stop" or action != "stop":
+                    game.StartRound()
+                    if not self.dealer:
+                        game.Blackjack()
+                    else:
+                        while action != "Stop" or action != "stop":
+                            action = input()
+                            if action not in 'HPShps' and action not in ['hit', 'Hit', 'Pat', 'pat', 'split', 'Split']:
+                                print("Excuse me, could you repeat that?")
+                            else:
+                                game.ApplyAction(action)
+
 
 while True:
     ritual = input()
